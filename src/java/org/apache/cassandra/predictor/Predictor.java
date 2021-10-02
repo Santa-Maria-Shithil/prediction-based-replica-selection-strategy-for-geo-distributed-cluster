42ca605cff94190fe087770dfd2a198c3b819dcd
Santa Maria Shithil <53658987+Santa-Maria-Shithil@users.noreply.github.com>
2021-10-02 16:36:03 +0600

    Update Predictor.java

--- src/java/org/apache/cassandra/predictor/Predictor.java
+++ src/java/org/apache/cassandra/predictor/Predictor.java
@@ -139,7 +139,7 @@ public class Predictor {
 		int qsize=queuesize2.get(key).get();
 	   //	logger.info("decrementing pending job inside predictor");
 		//String data = key.toString() + " " + Integer.toString(qsize) + " " +l + " " + stime+" "+"UPDATEREMOTE"+"\n";
-		String data = key.toString() + " " +l + " " + stime + " " + Integer.toString(qsize) +" "+ "UPDATEREMOTE"+"\n";
+		String data = key.toString() + " " +l + " " + stime + " " + Integer.toString(qsize) +" "+DynamicEndpointSnitch.getSeverity(key)+" "+ "UPDATEREMOTE"+"\n";
 		logger.info(data);
 		l=l-stime;
 		if(latencyEMA.containsKey(key))
